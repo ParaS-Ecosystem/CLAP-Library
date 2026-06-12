dce56b6b44c91ff7993f75ab2b29ede2263ef427
garadeaniket <129481361+garadeaniket@users.noreply.github.com>
2026-06-12 13:15:47 +0530

    updated level1.cpp

--- src/openblas/level1.cpp
+++ src/openblas/level1.cpp
@@ -15,64 +15,74 @@
 
 namespace clap {
 
-//Float COPY
+// Float COPY
 
- void OpenBlasBackend::OpenBlasBackend::copy(int64_t n, const float* x,  int64_t incx, float*  y, int64_t incy) {
-   clap_cblas_scopy(n, x, incx, y, incy);
+void OpenBlasBackend::OpenBlasBackend::copy(int64_t n, const float *x,
+                                            int64_t incx, float *y,
+                                            int64_t incy) {
+  clap_cblas_scopy(n, x, incx, y, incy);
 }
 
-//Double COPY
+// Double COPY
 
-void OpenBlasBackend::OpenBlasBackend::copy(int64_t n, const double* x, int64_t incx, double* y, int64_t incy) {
-   clap_cblas_dcopy(n, x, incx, y, incy);
+void OpenBlasBackend::OpenBlasBackend::copy(int64_t n, const double *x,
+                                            int64_t incx, double *y,
+                                            int64_t incy) {
+  clap_cblas_dcopy(n, x, incx, y, incy);
 }
 
-//Float SCAL
+// Float SCAL
 
-void OpenBlasBackend::scal(int64_t n, float a, float* x, int64_t incx) {
-   clap_cblas_sscal(n, a, x, incx);
+void OpenBlasBackend::scal(int64_t n, float a, float *x, int64_t incx) {
+  clap_cblas_sscal(n, a, x, incx);
 }
 
-//Double SCAL
+// Double SCAL
 
-void OpenBlasBackend::scal(int64_t n, double a, double* x, int64_t incx) {
-   clap_cblas_dscal(n, a, x, incx);
+void OpenBlasBackend::scal(int64_t n, double a, double *x, int64_t incx) {
+  clap_cblas_dscal(n, a, x, incx);
 }
 
-//Float axpy
+// Float axpy
 
-void OpenBlasBackend::axpy(int64_t n, float a, const float* x, int64_t incx, float* y, int64_t incy) {
-   clap_cblas_saxpy(n, a, x, incx, y, incy);
+void OpenBlasBackend::axpy(int64_t n, float a, const float *x, int64_t incx,
+                           float *y, int64_t incy) {
+  clap_cblas_saxpy(n, a, x, incx, y, incy);
 }
 
-//Double axpy
+// Double axpy
 
-void OpenBlasBackend::axpy(int64_t n, double a, const double* x, int64_t incx, double* y, int64_t incy) {
-   clap_cblas_daxpy(n, a, x, incx, y, incy);
+void OpenBlasBackend::axpy(int64_t n, double a, const double *x, int64_t incx,
+                           double *y, int64_t incy) {
+  clap_cblas_daxpy(n, a, x, incx, y, incy);
 }
 
-//Float swap
+// Float swap
 
-void OpenBlasBackend::swap(int64_t n, float* x, int64_t incx, float* y, int64_t incy) {
-   clap_cblas_sswap(n, x, incx, y, incy);
+void OpenBlasBackend::swap(int64_t n, float *x, int64_t incx, float *y,
+                           int64_t incy) {
+  clap_cblas_sswap(n, x, incx, y, incy);
 }
 
-//Double swap
+// Double swap
 
-void OpenBlasBackend::swap(int64_t n, double* x, int64_t incx, double* y, int64_t incy) {
-   clap_cblas_dswap(n, x, incx, y, incy);
+void OpenBlasBackend::swap(int64_t n, double *x, int64_t incx, double *y,
+                           int64_t incy) {
+  clap_cblas_dswap(n, x, incx, y, incy);
 }
 
-//Double dot
+// Double dot
 
-float OpenBlasBackend::dot(int64_t n, const float* x, int64_t incx, const float* y, int64_t incy) {
-    return clap_cblas_sdot(n, x, incx, y, incy);
-} 
-
-//Float dot
-
-double OpenBlasBackend::dot(int64_t n, const double* x, int64_t incx, const double* y, int64_t incy) {
-    return clap_cblas_ddot(n, x, incx, y, incy);
+float OpenBlasBackend::dot(int64_t n, const float *x, int64_t incx,
+                           const float *y, int64_t incy) {
+  return clap_cblas_sdot(n, x, incx, y, incy);
 }
 
+// Float dot
+
+double OpenBlasBackend::dot(int64_t n, const double *x, int64_t incx,
+                            const double *y, int64_t incy) {
+  return clap_cblas_ddot(n, x, incx, y, incy);
 }
+
+} // namespace clap

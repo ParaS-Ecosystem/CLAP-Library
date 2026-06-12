e0a5743088ea8096f6b1c54723892c55f9cbb645
garadeaniket <129481361+garadeaniket@users.noreply.github.com>
2026-06-12 09:33:22 +0000

    axpy_d.cpp added

---
+++ examples/level1/axpy_d.cpp
@@ -0,0 +1,59 @@
+
+#include <blas.hh>
+#include <chrono>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace clap;
+using namespace std::chrono;
+
+int main() {
+  int n;
+
+  printf("Enter vector size (double): ");
+  scanf("%d", &n);
+
+  printf("n = %d\n", n);
+
+  srand(time(NULL));
+
+  const double alpha = 2.0;
+
+  double *x = (double *)malloc(n * sizeof(double));
+  double *y = (double *)malloc(n * sizeof(double));
+
+  if (x == NULL || y == NULL) {
+    printf("malloc failed\n");
+    return 1;
+  }
+
+  for (int i = 0; i < n; i++) {
+    x[i] = (double)((rand() % 10) + 1);
+
+    y[i] = (double)((rand() % 10) + 1);
+  }
+
+  auto start = high_resolution_clock::now();
+
+  auto backend = clap::BlasFactory::create();
+
+  backend->axpy(n, alpha, x, 1, y, 1);
+
+  auto stop = high_resolution_clock::now();
+
+  printf("\nAfter AXPY:\n");
+
+  for (int i = 0; i < n; i++) {
+    printf("y[%d] = %lf\n", i, y[i]);
+  }
+
+  double total_seconds = duration<double>(stop - start).count();
+
+  printf("Total time : %.6f s\n", total_seconds);
+
+  free(x);
+  free(y);
+
+  return 0;
+}
